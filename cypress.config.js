5af21ea6adc19501096455ffbc4280b31bf0d4d1
luccasdasilvacandido <luccasdasilvacandido@gmail.com>
2025-01-23 14:03:49 -0300

    feat: Modificando o specPattern do Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "cypress/tests/**/*.cy.{js,jsx,ts,tsx}",
     baseUrl: "https://front.serverest.dev/",
     apiUrl: "https://serverest.dev",
     setupNodeEvents(on, config) {
