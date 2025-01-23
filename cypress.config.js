f99f0eb726af4df535faf92df674dd54f8479f01
luccasdasilvacandido <luccasdasilvacandido@gmail.com>
2025-01-23 11:59:03 -0300

    chore: Adicionando base url da api

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://front.serverest.dev/",
+    apiUrl: "https://serverest.dev",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
